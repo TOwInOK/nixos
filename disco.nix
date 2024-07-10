a00ae258e1eb5cf5da5a1e4f5e32061855d8e02e
TOwInOK <60252419+TOwInOK@users.noreply.github.com>
2024-07-10 21:33:24 +0700

    fix disco

--- disco.nix
+++ disco.nix
@@ -1,45 +1,47 @@
 {
-  disko.devices = {
-    disk = {
-      nvme = {
-        type = "disk";
-        device = "/dev/nvme0n1";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-                priority = 1;
-                name = "ESP";
-                size = "512M";
-                type = "EF00";
+    disco.devices = {
+        disk = {
+            nvme = {
+                type = "disk";
+                device = "/dev/nvme0n1";
                 content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
+                    type = "gpt";
+                    partitions = {
+                        ESP = {
+                            priority = 1;
+                            name = "ESP";
+                            size = "512M";
+                            type = "EF00";
+                            content = {
+                                type = "filesystem";
+                                format = "vfat";
+                                mountpoint = "/boot";
+                            };
+                        };
+                        root = {
+                            size = "100%";
+                            content = {
+                                type = "btrfs";
+                                extraArgs = [ "-f" ];
+                                subvolumes = {
+                                    "/rootfs" = {
+                                        mountpoint = "/";
+                                    };
+                                    "/home" = {
+                                        mountOptions = [ "compress=zstd" ];
+                                        mountpoint = "/home";
+                                    };
+                                    "/home/towinok/.config/home-manager" = { };
+                                    "/nix" = {
+                                        mountOptions = [ "compress=zstd" "noatime" ];
+                                        mountpoint = "/nix";
+                                    };
+                                };
+                            };
+                        };
+                    };
                 };
             };
-            root = {
-              size = "100%";
-              content = {
-                type = "btrfs";
-                extraArgs = [ "-f" ];
-                subvolumes = {
-                  "/rootfs" = {
-                    mountpoint = "/";
-                };
-                  "/home" = {
-                    mountOptions = [ "compress=zstd" ];
-                    mountpoint = "/home";
-                };
-                  "/home/towinok" = { };
-                  "/nix" = {
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                    mountpoint = "/nix";
-                };
-            };
-          };
         };
-      };
     };
-  };
 }
